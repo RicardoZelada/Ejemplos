6741c72013e3a7182aa858010d120ff345fd3cdc
ricardo <ricky.zelada.a@gmail.com>
2021-07-14 01:55:50 -0400

    Creacion de Slider
    Se probo slider con splide.js

--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,4 @@
-const btnMod = document.querySelector(".btn-enviar");
+/*const btnMod = document.querySelector(".btn-enviar");
 btnMod.addEventListener('click',()=>{
     let divResult = document.querySelector(".div-resultado");
 
@@ -39,8 +39,38 @@ console.log(personaje_2);
 
 let lista = [];
 lista.push(personaje_2);
-console.log(lista);
+console.log(lista);*/
 
 
 
 //------------------------------------------------
+
+//------ Slider  de headerResponsive ----------------
+var secondarySlider = new Splide( '#secondary-slider', {
+    rewind      : true,
+    fixedWidth  : 100,
+    fixedHeight : 64,
+    isNavigation: true,
+    gap         : 10,
+    focus       : 'center',
+    pagination  : false,
+    cover       : true,
+    breakpoints : {
+      '600': {
+        fixedWidth  : 70,
+        fixedHeight : 44,
+      }
+    }
+  } ).mount();
+
+  // Create the main slider.
+  var primarySlider = new Splide( '#primary-slider', {
+    type       : 'fade',
+    heightRatio: 0.5,
+    pagination : false,
+    arrows     : false,
+    cover      : true,
+  } );
+
+  // Set the thumbnails slider as a sync target and then call mount.
+  primarySlider.sync( secondarySlider ).mount();
